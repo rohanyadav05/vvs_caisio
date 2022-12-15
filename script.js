07c77e8f27843a832dfcb9254095fc42abb07a50
ROHAN YADAV <>
2022-12-15 07:09:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const from = document.getElementById("from")
+const to = document.getElementById("to")
+const inputNumber = document.getElementById("input-number")
+const resultBox = document.getElementById("result-box")
+
+document.getElementById("convert").addEventListener("click", () => {
+    //check from
+    let fromValue = from.value 
+    // check to
+    let toValue = to.value
+    // check input number
+   let inputValue = inputNumber.value
+   // convert input number "from" to "to"
+   let result = convertBase(fromValue, toValue, inputValue)
+   //show the result in result box
+   resultBox.innerText = result
+})
+
+function convertBase(fromBase, toBase, num) {
+    let d = parseInt(num, fromBase) // converting number into base10
+    let res = d.toString(toBase) //converting base10 to required base
+    return res
+}
+document.getElementById("swap").addEventListener("click", () => {
+    let fromValue = from.value 
+    let toValue = to.value
+   let inputValue = inputNumber.value
+  
+   let result = convertBase(toValue,fromValue, inputValue)
+   resultBox.innerText = result
+let temp = from.value
+    from.value = to.value
+    to.value = temp
+})
